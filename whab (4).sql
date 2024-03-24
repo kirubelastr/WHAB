452288590d12507df303a3c56633bc800c084cb5
kirubelastr <132957975+kirubelastr@users.noreply.github.com>
2024-03-24 22:40:32 +0300

    updated the dbtables

--- whab (4).sql
+++ whab (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2024 at 07:57 PM
+-- Generation Time: Mar 24, 2024 at 08:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `candles` (
   `id` int(11) NOT NULL,
   `name` varchar(10) NOT NULL,
-  `amount` decimal(6,4) NOT NULL
+  `amount` decimal(4,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -38,10 +38,10 @@ CREATE TABLE `candles` (
 --
 
 INSERT INTO `candles` (`id`, `name`, `amount`) VALUES
-(1, 'big', 9.2500),
-(2, 'amen', 7.2000),
-(3, 'medium', 6.0000),
-(4, 'color', 4.0000);
+(1, 'big', 9.25),
+(2, 'amen', 7.20),
+(3, 'medium', 6.00),
+(4, 'color', 4.00);
 
 -- --------------------------------------------------------
 
@@ -53,12 +53,20 @@ CREATE TABLE `inventory` (
   `id` int(11) NOT NULL,
   `discription` varchar(10) NOT NULL,
   `bag_number` int(11) NOT NULL,
-  `weight` decimal(7,3) NOT NULL,
-  `total` decimal(9,4) NOT NULL,
+  `weight` decimal(7,2) NOT NULL,
+  `total` decimal(9,2) NOT NULL,
   `place` varchar(25) NOT NULL,
   `remark` varchar(25) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `discription`, `bag_number`, `weight`, `total`, `place`, `remark`, `date`) VALUES
+(24, 'waxandoil', 21, 3.00, 63.00, 'sf', 'gg', '2024-03-22'),
+(25, 'waxandoil', 2, 3.00, 6.00, '1', '3', '2024-03-24');
 
 -- --------------------------------------------------------
 
@@ -104,7 +112,7 @@ ALTER TABLE `candles`
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
