61314ff949ea4ab3d8433693fc83dda7a6ce1b93
kirubelastr <132957975+kirubelastr@users.noreply.github.com>
2024-03-26 01:22:59 +0300

    db updated

--- whab (1).sql
+++ whab (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2024 at 06:41 PM
+-- Generation Time: Mar 25, 2024 at 11:22 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,7 +39,13 @@ CREATE TABLE `candleinventory` (
 --
 
 INSERT INTO `candleinventory` (`id`, `name`, `amount`, `date`) VALUES
-(1, 'big', 22, '2024-03-25');
+(2, 'big', 0, '2024-03-25'),
+(3, 'amen', 0, '2024-03-25'),
+(4, 'big', 0, '2024-03-25'),
+(5, 'big', 0, '2024-03-26'),
+(6, 'big', 0, '2024-03-26'),
+(7, 'big', 33, '2024-03-26'),
+(8, 'amen', 20, '2024-03-26');
 
 -- --------------------------------------------------------
 
@@ -85,17 +91,19 @@ CREATE TABLE `inventory` (
 --
 
 INSERT INTO `inventory` (`id`, `discription`, `bag_number`, `weight`, `total`, `place`, `remark`, `date`) VALUES
-(24, 'waxandoil', 21, 3.00, 63.00, 'sf', 'gg', '2024-03-22'),
-(25, 'waxandoil', 2, 3.00, 6.00, '1', '3', '2024-03-24'),
-(26, 'waxandoil', 2, 33.00, 66.00, 'ad', 'fff', '2024-03-25'),
-(27, 'waxandoil', 2, 3.00, 6.00, 'asf', 'vzx', '2024-03-25'),
-(28, 'waxandoil', 2, 4.00, 8.00, 'af', 'asf', '2024-03-25'),
-(29, 'waxandoil', 2, 2.00, 4.00, '1', '1', '2024-03-25'),
-(30, 'waxandoil', 3, 2.00, 6.00, 'd', 'd', '2024-03-25'),
-(31, 'waxandoil', 3, 2.00, 6.00, 'ee', 'e', '2024-03-25'),
-(32, 'waxandoil', 2, 3.00, 6.00, 'ava', 'afsaf', '2024-03-25'),
-(33, 'waxandoil', 3, 3.00, 9.00, '23', '32', '2024-03-25'),
-(34, 'waxandoil', 3, 4.00, 12.00, 'f', 'f', '2024-03-25');
+(24, 'waxandoil', 21, 3.00, 0.00, 'sf', 'gg', '2024-03-22'),
+(25, 'waxandoil', 2, 3.00, 0.00, '1', '3', '2024-03-24'),
+(26, 'waxandoil', 2, 33.00, 0.00, 'ad', 'fff', '2024-03-25'),
+(27, 'waxandoil', 2, 3.00, 0.00, 'asf', 'vzx', '2024-03-25'),
+(28, 'waxandoil', 2, 4.00, 0.00, 'af', 'asf', '2024-03-25'),
+(29, 'waxandoil', 2, 2.00, 0.00, '1', '1', '2024-03-25'),
+(30, 'waxandoil', 3, 2.00, 0.00, 'd', 'd', '2024-03-25'),
+(31, 'waxandoil', 3, 2.00, 0.00, 'ee', 'e', '2024-03-25'),
+(32, 'waxandoil', 2, 3.00, 0.00, 'ava', 'afsaf', '2024-03-25'),
+(33, 'waxandoil', 3, 3.00, 0.00, '23', '32', '2024-03-25'),
+(34, 'waxandoil', 3, 4.00, 0.00, 'f', 'f', '2024-03-25'),
+(35, 'waxandoil', 3, 5.00, 0.00, 'dadad', 'dads', '2024-03-25'),
+(36, 'waxandoil', 23, 33.00, 85.00, 'asf', 'wwfafs', '2024-03-26');
 
 -- --------------------------------------------------------
 
@@ -106,10 +114,19 @@ INSERT INTO `inventory` (`id`, `discription`, `bag_number`, `weight`, `total`, `
 CREATE TABLE `sales` (
   `id` int(11) NOT NULL,
   `candle_type` varchar(10) NOT NULL,
-  `amount` decimal(7,3) NOT NULL,
-  `total` decimal(8,4) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `place` varchar(15) NOT NULL,
   `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`id`, `candle_type`, `amount`, `place`, `date`) VALUES
+(0, 'amen', 0, 'cb', '2024-03-26'),
+(0, 'big', 0, 'm', '2024-03-26'),
+(0, 'big', 0, '2', '2024-03-26');
 
 --
 -- Indexes for dumped tables
@@ -141,7 +158,7 @@ ALTER TABLE `inventory`
 -- AUTO_INCREMENT for table `candleinventory`
 --
 ALTER TABLE `candleinventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `candles`
@@ -153,7 +170,7 @@ ALTER TABLE `candles`
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
